2cba10d9d1ee0a4cd39b9dc7637e49b218734341
ThalesChamone <72635068+ThalesChamone@users.noreply.github.com>
2021-06-21 19:16:21 -0300

    Add files via upload

---
+++ Exercicio_Salario/Exercicio_Salario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercicio_Salario
+{
+    class Exercicio_Salario
+    {
+        static void Main(string[] args)
+        {
+            Salario funcionario;
+
+            funcionario = new Salario();
+
+            Console.Write("Nome: ");
+            funcionario.Nome = Console.ReadLine();
+            Console.Write("Salario Bruto: " );
+            funcionario.SalarioBruto = double.Parse(Console.ReadLine());
+            Console.Write("Imposto: ");
+            funcionario.Imposto = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Funcionario: " + funcionario);
+            Console.WriteLine();
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            int aumento = int.Parse(Console.ReadLine());
+            funcionario.AumentoSalario(aumento);
+
+            Console.WriteLine();
+
+            Console.Write("Dados atualizados: " + funcionario);
+
+        }
+    }
+}
---
+++ Exercicio_Salario/Salario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio_Salario
+{
+    class Salario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentoSalario(int porcentagem)
+        {
+            SalarioBruto = SalarioBruto + (SalarioBruto * porcentagem / 100.0);
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", $" + SalarioLiquido().ToString("F2");
+        }
+    }
+   }
